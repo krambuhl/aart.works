f03278f0111ff43e9b7622bce7b07a8bb6124b04
Evan Krambuhl <evan.krambuhl@gmail.com>
2022-12-23 13:48:32 -0800

    shades

--- pages/sketch/21-shading.tsx
+++ pages/sketch/21-shading.tsx
@@ -9,22 +9,22 @@ import { PageHeader } from 'components/shared/PageHeader'
 import { Stack } from 'components/shared/Stack'
 import { tokens } from 'tokens'
 
+const frag = (a: TemplateStringsArray) => a
+const vert = (a: TemplateStringsArray) => a
+
 const baseBg: P5Color = [0 / 255, 0 / 255, 0 / 255, 255]
 const canvasSize = 512
 
-// const sides = 41 * 1
-// const padding = 32
-// const offset = 0
-
-const sides = 29
-const padding = 32
+const nodes = 11
+const padding = 48
 
 const gutter = -0.5
-const size = (canvasSize - padding * 2) / sides
+const offset = (padding / 2) - (nodes / 2)
+const size = (canvasSize - padding * 2) / nodes
 
 export const meta = {
-  title: 'Grid F',
-  date: '2022-11-07T00:00:00',
+  title: 'Shading',
+  date: '2022-11-08T00:00:00',
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -44,23 +44,24 @@ export default function Output() {
 
               shader = p.createShader(vs, fs)
 
-              store.frames = Array(Math.pow(sides, 2))
+              store.frames = Array(Math.pow(nodes, 2))
                 .fill(null)
                 .map((_, i) => ({
-                  x: i % sides,
-                  y: Math.floor(i / sides),
+                  x: i % nodes,
+                  y: Math.floor(i / nodes),
                 }))
             }}
             draw={(p, store) => {
               p.clear(...baseBg)
               p.noStroke()
+              // p.orbitControl()
               p.normalMaterial()
 
               const start = p.frameCount
               const length = store.frames.length
 
               p.shader(shader)
-              shader.setUniform('uFrameCount', start)
+              shader.setUniform('frame', start)
 
               for (let i = 0; i < length; i++) {
                 const pos = store.frames[i]
@@ -69,8 +70,8 @@ export default function Output() {
                 // const color = (pos.x + offset) * (pos.y + offset) + time
                 // const fill = p.color(rainbow[Math.floor(color % rainbow.length)])
 
-                const x = (pos.x - sides / 2) * size
-                const y = (pos.y - sides / 2) * size
+                const x = offset + (pos.x - nodes / 2) * size
+                const y = offset + (pos.y - nodes / 2) * size
                 const scale = size - gutter
 
                 p.push()
@@ -78,13 +79,25 @@ export default function Output() {
                 shader.setUniform('x', x)
                 shader.setUniform('y', y)
 
-                p.translate(x, y, (p.sin((x * y) + time / 10)) * ((p.sin(x * 0.2) * p.cos(y * 0.1)) * 4))
-                p.rotateX(p.sin(x / size / 4) + p.cos(time / 12))
-                p.rotateY(p.sin(y / size / 8) + p.cos(time / 16))
-                p.rotateZ((x * y) + (time / 10))
+                p.translate(x, y)
+                p.rotateX(
+                  p.sin(x * size / 4) +
+                  p.cos(time / 12)
+                )
+                p.rotateY(
+                  p.sin(y * size / 8) +
+                  p.cos(time / 16)
+                )
+                p.rotateZ(
+                  p.sin(
+                    x * y +
+                    (time / 10)
+                  )
+                )
+
                 // p.torus(scale * 0.5, scale * 0.4)
                 // p.box(scale * 1.8, scale * 1.8, scale * 1.8, 420, 420)
-                p.sphere(scale)
+                p.sphere(scale * 0.4)
 
                 p.pop()
               }
@@ -96,37 +109,44 @@ export default function Output() {
   )
 }
 
-const vs = `
+const vs = vert`
   attribute vec3 aPosition;
   attribute vec2 aTexCoord;
   attribute vec3 aNormal;
 
   uniform mat4 uProjectionMatrix;
   uniform mat4 uModelViewMatrix;
+  
+  uniform float frame;
   uniform float x;
   uniform float y;
 
   varying vec2 vTexCoord;
   varying vec3 vNormal;
+
   void main() {
-    vec4 positionVec4 = vec4(aPosition, 2.0);
+    vec4 positionVec4 = vec4(aPosition, 1.0);
     float frequency = 2.0;
-    float amplitude = 1.;
-    float distortion = sin(positionVec4.x * frequency + 1.0 * 0.1);
-    // positionVec4.x += distortion * ((1.) / 3.01) * aNormal.x * amplitude;
+    float amplitude = 1.0;
+
     vNormal = aNormal;
     gl_Position = uProjectionMatrix * uModelViewMatrix * positionVec4;
     vTexCoord = aTexCoord;
   }
 `
 
-const fs = `
+const fs = frag`
   precision mediump float;
+
+  uniform float frame;
+  uniform float x;
+  uniform float y;
+
   varying vec2 vTexCoord;
   varying vec3 vNormal;
 
   void main() {
-    vec3 color = vNormal * 0.5 + 0.5;
-    gl_FragColor = vec4(color, 1.5);
+    vec3 color = vec3(frame, frame, frame);
+    gl_FragColor = vec4(vNormal, color);
   }
 `
